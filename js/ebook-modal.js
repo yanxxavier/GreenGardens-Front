78b796392687e93f9b939a751b1597124e2d6493
yanxxavier <yanxavierlindolfo@gmail.com>
2024-06-21 03:10:55 -0300

    Feat: Ebook Js

--- js/ebook-modal.js
+++ js/ebook-modal.js
@@ -1,22 +1,106 @@
 export default function initEbookModal() {
+    const modalBox = document.querySelector('.modal_box');
+    const modalForms = document.getElementById('modalForm')
+    
+    //Abrir o modal
+    
+    const openModalBtn = document.getElementById('buttonOpenModal');
+    openModalBtn.addEventListener('click', () => {
+        modalBox.style.display = 'flex';
+        modalForms.style.display = 'block';
+    })
+    
+    //Fechar o modal
+    
+    function closeModal(modal) { 
+        modal.style.display = 'none';
+    }
+    
+    const closeModalBtn = document.getElementById('backModalBtn');
+    closeModalBtn.addEventListener('click', (event) => {
+        event.preventDefault();
+        closeModal(modalBox);
+    });
+    
+    //Fechar modal com outise click
+    
+    modalBox.addEventListener('click', (event) => {
+        if(event.target === modalBox) {
+            closeModal(modalBox)
+        }
+    })
+    
+    
+    // Verifica os input e abre modal download com sucesso
+    
+    const modalInputName = document.getElementById('modalName');
+    addInputListener(modalInputName);
+    const modalInputPhone = document.getElementById('modalTelefone');
+    addInputListener(modalInputPhone);
+    const modalInputEmail = document.getElementById('modalEmail');
+    addInputListener(modalInputEmail)
+    
+    function addInputListener(input) {  
+        input.addEventListener('input', (event) => {   // Adicionar um Listener pra cada input, sem precisar repetir
+            let inputValue = event.target.value;
+    
+        })
+    }
+    
+    const downloadFinished = document.getElementById('downloadFinished');
+    
+    const downloadBtn = document.getElementById('dowloadModalBtn');
+    downloadBtn.addEventListener('click', (event) => {
+        event.preventDefault();
 
+        const verified = modalInputName.value.length  > 3 && modalInputPhone.value.length > 8 && modalInputEmail.value.length > 7;
+        
+        if(verified) {
+            modalForms.style.display = 'none';
+            downloadFinished.style.display = 'flex';
+
+            Toastify({ //Lib de alert 
+                text: "Sucesso!",
+                duration: 3000,
+                close: true,
+                gravity: "top", 
+                position: "right", 
+                stopOnFocus: false, 
+                style: {
+                  background: "#242C12",
+                }
+            }).showToast();
+
+            modalInputName.value = '';
+            modalInputPhone.value = ''; // Resetar os campos de input
+            modalInputEmail.value = '';
+    
+        }else {
+            Toastify({ //Lib de alert 
+                text: "Preencha os campos corretamente!",
+                duration: 3000,
+                close: true,
+                gravity: "top", 
+                position: "right",
+                stopOnFocus: true, 
+                style: {
+                  background: "red",
+                },
+                
+              }).showToast();
+        }
+    
+    })
+    
+    
+    // Fechar o a tela de dowload com sucesso
+    
+    const closeDowloadBtn = document.getElementById('CloseDowloadBtn');
+    closeDowloadBtn.addEventListener('click', (event) => {
+        event.preventDefault();
+        closeModal(modalBox); // Fechar o modal inteiro
+        closeModal(downloadFinished); // Fechar a tela de download
+    })
 }
 
 
-const modalBox = document.querySelector('.modal_box');
-
-//Abrir o modal
-
-const openModalBtn = document.getElementById('buttonOpenModal');
-openModalBtn.addEventListener('click', () => {
-    modalBox.style.display = 'flex';
-})
-
-//Fechar o modal
-
-function closeModal() {
-    modalBox.style.display = 'none';
-}
-
-const closeModalBtn = document.getElementById('backModalBtn');
-closeModalBtn.addEventListener('click', closeModal)
