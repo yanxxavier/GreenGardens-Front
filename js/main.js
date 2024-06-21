0500cc712814085e017d55fa69d7192a9a214846
yanxxavier <yanxavierlindolfo@gmail.com>
2024-06-21 17:54:26 -0300

    Fix: NavBar Links

--- js/main.js
+++ js/main.js
@@ -2,8 +2,10 @@ import initSliderHome from "./home-slider.js";
 import initValoresCarrossel from "./valoresCarrossel.js";
 import initEbookModal from "./ebook-modal.js";
 import initAnimationScroll from "./anima-scroll.js";
+import initScrollLinks from "./links-scroll.js";
 
 initSliderHome();
 initAnimationScroll();
+initScrollLinks();
 initValoresCarrossel();
 initEbookModal();
