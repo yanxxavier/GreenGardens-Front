e8d7cab8d7af9ec02698878199c7e04e3bddb05f
yanxxavier <yanxavierlindolfo@gmail.com>
2024-06-22 01:31:41 -0300

    Feat: Responsividade valores

--- js/valoresCarrossel.js
+++ js/valoresCarrossel.js
@@ -11,12 +11,32 @@ export default function initValoresCarrossel() {
         slideShadows: true,
         scale : 0,
       },
+      breakpoints: {
+          
+        1000: {
+          slidesPerView: 2.5,
+          spaceBetween: 10
+        },
+        800: {
+          slidesPerView: 2,
+          spaceBetween: 10
+        },
+        600: {
+          slidesPerView: 1.5,
+          spaceBetween: 10
+        },
+        300: {
+          slidesPerView: 1,
+          spaceBetween: 20
+        }
+      },
       
         direction: 'horizontal',
         loop: true,
         slidesPerView: 3,
         spaceBetween: 20,
         slidesPerGroup: 1,
+
       
       });
 }   
